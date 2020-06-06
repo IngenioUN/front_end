4d477696c2122f52e5943f6691217670748715a9
cdrinconm <cdrinconm@unal.edu.co>
2020-06-05 23:28:28 -0500

    Heroku desplegar

--- src/store.js
+++ src/store.js
@@ -5,10 +5,11 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-    backURL: 'http://localhost:3000'
+    //backURL: 'http://localhost:3000'
+    backURL: 'https://ingeniobackend.herokuapp.com'
   },
   mutations:{
-    
+
   },
   actions: {
 
