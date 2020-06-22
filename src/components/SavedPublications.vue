cdcd5a2d5514bc81adb3ea38f098e73bc29e7723
cdrinconm <cdrinconm@unal.edu.co>
2020-06-22 14:38:45 -0500

    ya trae publicacion dependiendo de categoria

--- src/components/SavedPublications.vue
+++ src/components/SavedPublications.vue
@@ -1,17 +1,20 @@
 <template>
     <div class="divlogin">
       <div class="accordion" id="accordionExample">
-        <div class="card" v-for="item of savedPublication" :key="item.id">
+        <div class="card" v-for="item of categories" :key="item.id">
           <div class="card-header" id="headingOne">
             <h2 class="mb-0">
-              <button class="btn btn-link btn-block text-left " type="button" data-toggle="collapse" :data-target="'#' + item.Name" aria-expanded="true" aria-controls="collapseOne">
-                {{item.Name}}
+              <button class="btn btn-link btn-block text-left" type="button" data-toggle="collapse" :data-target="'#' + item.name" aria-expanded="true" aria-controls="collapseOne" @click="set(item._id,item.name); getSaveP()">
+                {{item.name}}
               </button>
             </h2>
           </div>
-          <div :id="item.Name" class="collapse" aria-labelledby="headingOne" data-parent="#accordionExample"  v-for="itemS of items.savedPublications" :key="itemS.id">
-            <div class="card-body" >
-              Title: {{itemS.title}}
+          <div :id="item.name" class="collapse" aria-labelledby="headingOne" data-parent="#accordionExample">
+            <div class="card-body" v-for="itemS of publications" :key="itemS.id">
+              Title: {{itemS.title}}<br/>
+              Description: {{itemS.abstract}}<br/>
+              Categories: {{itemS.listCategories}}<br/>
+              Author: {{itemS.authorId}}<br/>
             </div>
           </div>
         </div>
@@ -29,41 +32,60 @@ export default {
   components: {},
   data: function (){
     return {
-      savedPublication: {
-        _1 :{
-          Name: 'Systems',
-          Description: 'Hi, Im systems'
-        },
-        _2:{
-          Name: 'Chemistry',
-          Description: 'Hi, Im chemistry'
-        },
-        _3:{
-          Name: 'Civil',
-          Description: 'Hi, Im civil'
-        },
-        _4:{
-          Name: 'Industrial',
-          Description: 'Hi, Im industrial'
-        }
-      },
-      items:[]
+      categories:{},
+      items:{},
+      publications:[],
+      categoryId:'',
+      categoryName:''
     }
+  },
+  created: function(){
+    axios
+		.get( this.$store.state.backURL + '/category/get-all-categories',)
+		.then( response => {
+			if( response.status !== 201 ){
+				alert( "Error en la autenticación" );
+			}else{
+        this.categories = response.data;
+			}
+		})
+		.catch( error => {
+			if( error.response.status === 400 ){
+				alert( "Credenciales incorrectas" );
+			}else{
+				alert( "¡Parece que hubo un error de comunicación con el servidor!" );
+			}
+    });
   },
   mounted() {
     this.getSaveP();
   },
   methods:{
+    set(idnuevo,namenuevo){
+      this.categoryId = idnuevo;
+      this.categoryName = namenuevo;
+    },
     selectCategory(item){
       this.Category = item;
       console.log(this.Category);
     },
     getSaveP() {
+      this.publications=[];
+      var publicat = {};
       axios
       .get(this.$store.state.backURL + "/user/get-save-publication")
       .then(response => {
         this.items = response.data;
         console.log(this.items);
+        for (let item in this.items.savedPublications){
+          for (let item2 in this.items.savedPublications[item].listCategories){
+               if(this.items.savedPublications[item].listCategories[item2] == this.categoryId){
+                 console.log(this.items.savedPublications[item]);
+                 publicat = this.items.savedPublications[item];
+                 this.publications.push(publicat);
+              }
+          }
+        }
       })
       .catch( error => {
         if( error.response.status === 400 ){
