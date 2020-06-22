174e758d9bf741362b0a7eba3abcc4d88f9c7bb0
cdrinconm <cdrinconm@unal.edu.co>
2020-06-22 17:54:43 -0500

    Heroku

--- src/store.js
+++ src/store.js
@@ -6,7 +6,7 @@ Vue.use(Vuex);
 export default new Vuex.Store({
   state: {
     //backURL: 'http://localhost:3000'
-    backURL: 'http://ingeniobackend.herokuapp.com'
+    backURL: 'https://ingeniobackend.herokuapp.com'
   },
   mutations:{},
   actions: {}
